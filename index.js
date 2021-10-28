6c8a53547c319ae3bdb589ef98bbb8fe2150ea8e
Destiny Bishop <bishde5v@gmail.com>
2021-10-27 20:27:00 -0500

    second atempt to commit

---
+++ index.js
@@ -0,0 +1,68 @@
+let message = document.querySelector("#message")
+
+let addMovie = (event) => {
+
+    event.preventDefault();
+
+    let inputField = document.querySelector("input"); 
+
+    let movie = document.createElement('Li');
+
+    let movieTitle = document.createElement('span');
+
+    movieTitle.textContent = inputField.value;
+
+    movieTitle.addEventListener("click", crossOffMovie);
+
+    movie.appendChild(movieTitle);
+
+    let deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "x";
+
+    deleteBtn.addEventListener("click", deleteMovie);
+
+    movie.appendChild(deleteBtn);
+
+    let list = document.querySelector('ul');
+
+    list.appendChild(movie);
+
+    inputField.value = "";
+
+};
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", addMovie);
+
+let deleteMovie = (event) => {
+
+    event.target.parentNode.remove();
+
+    message.textContent = `${event.target.parentNode.firstChild.textContent} Deleted`;
+    console.log(event.target.parentNode.textContent);
+
+    revealMessage();
+};
+
+let crossOffMovie = (event) => {
+
+    event.target.classList.toggle("checked");
+
+    if (event.target.classList.contains("checked")){
+        message.textContent = `${event.target.textContent} Watched!`;
+    }else{
+        message.textContent = `${event.target.textContent} added back!`;
+}
+revealMessage();
+
+};
+
+let revealMessage = () => {
+    message.classList.remove("hide");
+    setTimeout(()=> {
+        message.classList.add("hide");
+
+    } , 1000)
+}
